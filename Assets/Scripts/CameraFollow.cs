e4d4971a94276b87229db26f6198b5c658986ff7
Phillips Albright <55517109+phillipsalbright@users.noreply.github.com>
2023-08-28 22:07:31 -0400

    camera movement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -19,7 +19,7 @@ public class CameraFollow : MonoBehaviour
         Vector3 newPosition = player.transform.position + new Vector3(0, 0, -10);
         Debug.Log(newPosition);
         newPosition = new Vector3(Mathf.Clamp(newPosition.x, MinBounds.x, MaxBounds.x), Mathf.Clamp(newPosition.y, MinBounds.y, MaxBounds.y), newPosition.z);
-        this.transform.position = Vector3.Lerp(this.transform.position, newPosition, Time.deltaTime  * 4);
+        this.transform.position = Vector3.Lerp(this.transform.position, newPosition, .1f);
        // Vector3 Velocity = Vector3.zero;
       ////  this.transform.position = Vector3.SmoothDamp(this.transform.position, newPosition, ref Velocity, Time.deltaTime);
     }
